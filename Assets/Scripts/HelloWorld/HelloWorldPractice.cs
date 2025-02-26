086f3691c6fe325cd5f66cf139613a8e08cd0d72
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-02-26 17:13:54 +0900

    1. Hello World 구현

---
+++ Assets/Scripts/HelloWorld/HelloWorldPractice.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWorldPractice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}
+
+/*
+[output] : 콘솔창에 아래 내용을 출력하세요
+Hello Unity!!!
+123456789
+*/
