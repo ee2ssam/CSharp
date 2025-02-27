c9f08fd350ca461df3d657d03342f627b4f26350
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-02-27 14:18:15 +0900

    1.  기본문법, 스타일 2.  변수

---
+++ Assets/Scripts/Variable/VariablePratice.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class VariablePratice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+}
+
+
+/*
+[Q]
+변수: first, second, third 정의하고 값 대입하여 아래내용 출력
+
+[output]
+10,20,30
+*/
