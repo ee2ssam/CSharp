c0f908f947aefcfb79e9a6e3d4f0e036b27d410d
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-02-28 13:18:20 +0900

    1. number 데이터 타입 예제 2. char, bool, string 데이터 타입 예제

---
+++ Assets/Scripts/Number/RealNumberPractice.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class RealNumberPractice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float f = 3.14f;
+        double d = 3.14;
+        decimal m = 3.14m;
+
+
+        Debug.Log("float: " + f);
+        Debug.Log("double: " + d);
+        Debug.Log("decimal: " + m);
+    }
+}
+
+
+/*
+[Q]
+실수형 값 float 3.14, double 3.14, decimal 3.14 값 출력하기
+
+[output]
+float: 3.14
+double: 3.14
+decimal: 3.14
+*/
--- Assets/Scripts/Variable/VariablePratice.cs
+++ Assets/Scripts/Variable/VariablePratice.cs
@@ -5,7 +5,16 @@ public class VariablePratice : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //[1] 변수 선언
+        int first, second, third;
+
+        //[2] 변수에 값 저장
+        first = 10;
+        second = 20;
+        third = 30;
+
+        //[3] 변수 사용
+        Debug.Log(first + "," + second + "," + third);
     }
 }
 
