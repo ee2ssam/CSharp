3f6ef3720133d2de09203f414d24eac9dfe0f8cb
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-09-05 14:15:52 +0900

    1. 실수형, 불형, 문자열, 문자 변수 예제 2. 연산자 예제

---
+++ Assets/Scripts/06Operator/OperatorDescription.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+// Operator(연산자) : +, -, *, / 등 연산을 진행하는 키워드
+public class OperatorDescription : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        /*Debug.Log(3 + 5);   //결과는:
+        Debug.Log(3 - 5);   //결과는:
+        Debug.Log(3 * 5);   //결과는:
+        Debug.Log(3 / 5);   //결과는:*/
+
+        /*int value = 0;
+
+        value = 8;      //value 변수에 8을 저장
+        value = +value; // 
+        Debug.Log(value);
+
+        value = -8;
+        value = +value; //
+        Debug.Log(value);
+
+        value = 8;
+        value = -value; //
+        Debug.Log(value);
+
+        value = -8;
+        value = -value; //
+        Debug.Log(value);*/
+
+        int i = 5;
+        int j = 3;
+        int k;
+
+        k = i + j;  //
+        Debug.Log(k);
+
+        k = i - j;  //
+        Debug.Log(k);
+
+        k = i * j;  //
+        Debug.Log(k);
+
+        k = i / j;  //
+        Debug.Log(k);
+
+        k = i % j;  //
+        Debug.Log(k);
+
+    }
+}
+
+/*
+단항 연산자 : +, -
+이항 연산자 : +, - , *, /, %
+삼항 연산자
+
+
+
+
+
+*/
+
---
+++ Assets/Scripts/07AssingmentOperator/PrefixOperator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+//전위 증감 연산자 : 같은 라인에서 연산하는 우선 순위가 가장 높다, 가장 먼저 연산
+//후위 증감 연산자 : 같은 라인에서 연산하는 우선 순위가 가장 낮다, 가장 늦게 연산
+public class PrefixOperator : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int i = 3;
+        int j = ++i;
+        Debug.Log($"j: {j}");   //
+
+        int x = 3;
+        int y = x++;
+        Debug.Log($"x: {x}");   //
+        Debug.Log($"y: {y}");   //
+    }
+}
