b41d15d96d701f61566efd78a025f453b5c8a3e6
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-09-16 16:05:01 +0900

    1. 제네릭 예제 2. 링크 예제 3. 네임스페이스 예제

---
+++ Assets/Scripts/26Namespace/Bar/Car.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+//Bar 네임스페이 선언
+namespace Bar
+{
+    public class Car
+    {
+        public void Go()
+        {
+            Debug.Log("Bar 네임스페이스의 Car클래스의 Go() 호출");
+        }
+    }
+}
+
