00b071c1aca62111beff06b65166cd3ccd4c7636
DESKTOP-5V3FRMH\M <ee2ssam@gmail.com>
2025-09-24 15:13:06 +0900

    1. 제네릭 클래스만들기 2. 연습문제 예제

---
+++ Assets/Scripts/37GenericClass/Cup.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Genenric
+{
+    public class Water { }
+
+    public class Coffee { }
+
+    //클래스이름<T> 형태로 제네릭 클래스 선언
+    public class Cup<T>
+    {
+        //T타입의 속성 멤버
+        public T Content { get; set; }
+    }
+}
+
